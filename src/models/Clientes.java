d55122a9b194de4211c9a8c2189c2e8d4f51fdeb
Matheus Augusto Santos Martinelli <44898398+MartinelliNTZ@users.noreply.github.com>
2022-11-09 19:58:04 -0300

    class  Clientes.java

--- src/models/Clientes.java
+++ src/models/Clientes.java
@@ -5,4 +5,17 @@ package models;
  * 09 /11/ 2022
  */
 public class Clientes {
+    private String nome;
+
+    public Clientes(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
