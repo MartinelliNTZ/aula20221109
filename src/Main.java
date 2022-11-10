9b91ab07dd208eaa48cd33e1e2e6181fa4dc2601
Matheus Augusto Santos Martinelli <44898398+MartinelliNTZ@users.noreply.github.com>
2022-11-09 22:51:29 -0300

    Contas.java

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,13 @@ public class Main {
         System.out.println("Hello world Nation!");
         System.out.println("Hello world nada! 898adsased98");
         //asdasdasd
+        link();
+    }
+    public void link(){
+
+    }
+    void f(){
+
+        link();
+    }
 }
---
+++ src/cliente.java
@@ -0,0 +1,14 @@
+/**
+ * @author Matheus Martinelli
+ * 09 /11/ 2022
+ */
+public class cliente {
+    void f(){
+
+        link();
+    }
+
+    void link(){
+
+    }
+}
